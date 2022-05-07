6f282a2a73a88cc5f13bcedbea6079789704acd9
Noamcm <noam.cohen.m@gmail.com>
2022-05-07 23:56:47 +0300

    many css

--- app.js
+++ app.js
@@ -34,6 +34,7 @@ function hideAllPages(){
 	$("#settings").hide();
 	$("#about").hide();
 	$("#game").hide();
+	$("#enter").hide();
 }
 
 // set hide and show functions for menu:
@@ -58,6 +59,12 @@ function showLogin(){
 	hideAllPages();
 	$('#homeOption').show();
 	$("#login").show();
+}
+
+function enterGame(){
+	hideAllPages();
+	$('#homeOption').show();
+	$("#enter").show();
 }
 
 function showSettings(){
