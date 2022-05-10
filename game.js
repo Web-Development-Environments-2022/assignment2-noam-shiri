a78e9cbff0164564bbb17c434f376066d7c58e94
Noamcm <noam.cohen.m@gmail.com>
2022-05-10 16:08:50 +0300

    colorful balls YYAAAYY

--- game.js
+++ game.js
@@ -8,12 +8,20 @@ var monsters;
 var color5points;
 var color15points;
 var color25points;
-var pacmanDirection='pacmanRight'
+var points5radio;
+var points15radio;
+var points25radio;
+var pacmanDirection='pacmanRight';
+var colors = {"Yellow": "#FFFD98" , "Green": "#D0F3B8" , "Blue": "#B8D7F3" , "Pink": "#F3B8F1" , "Purple": "#D6B8F3"}
 function Start() {
+	console.log(color5points, color15points ,color25points)
+	console.log(points5radio, points15radio ,points25radio)
 	score = 0;
-	pac_color = "yellow";
-	var cnt = 100;
 	var food_remain = food_requested;
+	var ball5 = Math.floor(food_requested*0.6);
+	var ball15 = Math.floor(food_requested*0.3);
+	var ball25 = Math.floor(food_requested*0.1);
+	ball5+=food_requested - (ball5+ball15+ball25)
 	start_time = new Date();
 	board=[[4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4],
 		[4,0,0,0,0,0,0,4,0,0,0,4,4,4,4,4,4,0,0,0,4,0,0,0,0,0,0,0,0,4],
@@ -34,21 +42,21 @@ function Start() {
 	console.log(emptyCell);
 	shape.i = emptyCell[0];
 	shape.j = emptyCell[1];
-	board[emptyCell[0]][emptyCell[1]] = 2;
-	for (var i = 0; i < 15; i++) {
-		for (var j = 0; j < 30; j++) {
-			var randomNum = Math.random();
-			if ((randomNum <= (1.0 * food_remain) / cnt)&&(board[i][j] == 0)) { // randomNum <= 0.5-0.9
-				food_remain--;
-				board[i][j] = 1;
-			}
-			cnt--;
-		}
-	}
-	while (food_remain > 0) {
-		var emptyCell = findRandomEmptyCell(board);
+	board[emptyCell[0]][emptyCell[1]] = 5;
+	while (ball5 > 0) {
+		emptyCell = findRandomEmptyCell(board);
 		board[emptyCell[0]][emptyCell[1]] = 1;
-		food_remain--;
+		ball5--;
+	}	
+	while (ball15 > 0) {
+		emptyCell = findRandomEmptyCell(board);
+		board[emptyCell[0]][emptyCell[1]] = 2;
+		ball15--;
+	}	
+	while (ball25 > 0) {
+		emptyCell = findRandomEmptyCell(board);
+		board[emptyCell[0]][emptyCell[1]] = 3;
+		ball25--;
 	}
 	keysDown = {};
 	addEventListener(
@@ -103,16 +111,26 @@ function Draw() {
 		  var center = new Object();
 		  center.y = i * 30 + 15;
 		  center.x = j * 30 + 15;
-		  if (board[i][j] == 2) {
+		  if (board[i][j] == 5) {
 			var pacmanimg = new Image();
 			pacmanimg.src = './pictures/'+pacmanDirection+'.png';
 			context.drawImage(pacmanimg, center.x-15, center.y-15,30, 30);
-			context.draw
-		  } else if (board[i][j] == 1) {
-			context.beginPath();
-			context.arc(center.x, center.y, 15, 0, 2 * Math.PI); // circle
-			context.fillStyle = "black"; //color food
-			context.fill();
+			context.draw;
+		} else if (board[i][j] == 1) {
+		  context.beginPath();
+		  context.arc(center.x, center.y, 5, 0, 2 * Math.PI); // circle
+		  context.fillStyle = colors[color5points]; //color food 5
+		  context.fill();
+		} else if (board[i][j] == 2) {
+		  context.beginPath();
+		  context.arc(center.x, center.y, 5, 0, 2 * Math.PI); // circle
+		  context.fillStyle = colors[color15points]; //color food 15
+		  context.fill();
+		} else if (board[i][j] == 3) {
+		  context.beginPath();
+		  context.arc(center.x, center.y, 5, 0, 2 * Math.PI); // circle
+		  context.fillStyle = colors[color25points]; //color food 25
+		  context.fill();
 		  } else if (board[i][j] == 4) {
 			context.beginPath();
 			context.rect(center.x - 15, center.y - 15, 30, 30);
@@ -153,7 +171,7 @@ function UpdatePosition() {
 	if (board[shape.i][shape.j] == 1) { //TODO : change score according to food
 		score++;
 	}
-	board[shape.i][shape.j] = 2;
+	board[shape.i][shape.j] = 5;
 	var currentTime = new Date();
 	time_elapsed = (currentTime - start_time) / 1000;
 	if (score == 100) {
